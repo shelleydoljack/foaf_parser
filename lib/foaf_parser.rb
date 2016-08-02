44a8956db4bc2708d2a470ffad319ef1972ac5b4
Shelley Doljack <sdoljack@stanford.edu>
2016-08-02 15:01:36 -0700

    Adds blank lines for readability.

--- lib/foaf_parser.rb
+++ lib/foaf_parser.rb
@@ -1,8 +1,10 @@
 class FoafParser
   attr_accessor :url
+
   def speak
     "I'm FoafParser!"
   end
+
   def initialize(rdf_url="http://stanford.edu/~sdoljack/sdoljack_foaf.rdf")
     @url=rdf_url
   end
